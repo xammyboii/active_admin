482389154bde3f153b5202867c4771bcdeafab86
xammyboii <xavieryabados.09@gmail.com>
2023-10-31 21:21:23 -0500

    registered breed and horse models

---
+++ app/admin/horses.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Horse do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :age, :top_speed, :number_of_legs, :breed_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :age, :top_speed, :number_of_legs, :breed_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
